c143de6d29a5fafea185cc8d3960ee5db0e187f6
yongjingmao <32618033+yongjingmao@users.noreply.github.com>
2022-09-25 13:00:08 +1000

    Update setup.py

--- submissions/team_UQMAO/setup.py
+++ submissions/team_UQMAO/setup.py
@@ -4,19 +4,16 @@ with open("README.md", "r") as readme_file:
     long_description = readme_file.read()
 
 setuptools.setup(
-    name = "HTSDS_GEE",
+    name = "HT-SDS",
     version = "0.0.1",
     author = "Yongjing Mao",
     author_email = "maomao940405@gmail.com",
     description = "A GEE based python package to derive shoreline position at high tide",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    url = "https://bitbucket.csiro.au/projects/DIG/repos/optical_sar_blending",
-    package_dir = {"": "algorithms\HTSDS_GEE"},
-    packages = setuptools.find_packages(include=["HTSDS_GEES", "HTSDS_GEE.*"]),
+    package_dir = {"": "submissions\team_UQMAO"},
+    packages = setuptools.find_packages(),
     python_requires = ">=3.7",
     install_requires = [
-		"pandas == 1.1.0"
+	"pandas == 1.1.0"
         "earthengine-api == 0.1.270"
         "geojson == 2.5.0"
     ]
